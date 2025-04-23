e7577deb4feaf27c33bf7ecdfaffd1186ca69498
nathertan <ndihyan@gmail.com>
2025-04-23 18:11:33 +0700

    Update: Created header and disabling dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <header>
+  <Header></Header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -14,7 +16,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="header">
+    <div class="header-logo">Elemes.id</div>
+    <nav class="header-nav">
+      <a href="#" class="header-nav-link">Home</a>
+      <a href="#" class="header-nav-link">About</a>
+      <a href="#" class="header-nav-link">Promotions</a>
+      <a href="#" class="header-nav-link">Blogs</a>
+      <a href="#" class="header-nav-link">Contact Us</a>
+    </nav>
+    <div class="header-auth">
+      <button class="header-auth-login">Masuk</button>
+      <button class="header-auth-register">Daftar Sekarang</button>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Header',
+})
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
